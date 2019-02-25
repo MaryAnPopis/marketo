cfd500b7918920f57da2a41e214dccef90decd55
Mariam <maryanpopis@hotmail.com>
2019-02-25 13:57:50 -0600

    fetch products by category

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -2,12 +2,17 @@ import { API_URL } from '../services'
 
 export const INIT_STATE = 'INIT_STATE'
 export const FETCH_PRODUCT_SUCCESS = 'FETCH_PRODUCT_SUCCESS'
+export const FETCH_PRODUCT_BY_CATEGORY_SUCCESS = 'FETCH_PRODUCT_BY_CATEGORY_SUCCESS'
 export const FETCH_PRODUCT_FAILURE = 'FETCH_PRODUCT_FAILURE'
 export const FETCH_PRODUCT_BEGIN = 'FETCH_PRODUCT_BEGIN'
 
 export const fetchProductsSuccess = product => ({
   type: FETCH_PRODUCT_SUCCESS,
   payload: { product },
+})
+export const fetchProductsByCategorySuccess = products => ({
+  type: FETCH_PRODUCT_BY_CATEGORY_SUCCESS,
+  payload: { products },
 })
 export const fetchProductsFailure = error => ({
   type: FETCH_PRODUCT_FAILURE,
@@ -26,6 +31,20 @@ export const fetchProduct = id => {
       .then(res => res.json())
       .then(json => {
         dispatch(fetchProductsSuccess(json))
+        return json
+      })
+      .catch(error => dispatch(fetchProductsFailure(error)))
+  }
+}
+
+export const fetchProductByCategory = id => {
+  return dispatch => {
+    dispatch(fetchProductsBegin())
+    return fetch(`${API_URL}/products/category/${id}?page=0&size=8/`)
+      .then(handleErrors)
+      .then(res => res.json())
+      .then(json => {
+        dispatch(fetchProductsByCategorySuccess(json))
         return json
       })
       .catch(error => dispatch(fetchProductsFailure(error)))
