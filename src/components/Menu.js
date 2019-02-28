6fb40a7f22b735a3966d293625dc032e9c7fbe62
Mariam <maryanpopis@hotmail.com>
2019-02-28 14:09:10 -0600

    menu responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Link, withRouter, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import MediaQuery from 'react-responsive'
 
 import colors from '../style/colors'
 import logo from '../img/logo.svg'
@@ -20,6 +21,7 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import Grid from '@material-ui/core/Grid'
 
 import { fetchCategories } from '../actions/categoryActions'
+import CustomeDrawer from '../components/Drawer'
 
 const styles = theme => ({
   badge: {
@@ -108,7 +110,7 @@ export class Menu extends Component {
     if (this.state.redirect) {
       return <Redirect to="/" />
     }
-
+    let width = window.innerWidth
     return (
       <div>
         <Style.Container_User>
@@ -121,11 +123,16 @@ export class Menu extends Component {
           <Style.Container className={classNames(classes.layout)}>
             <Grid container className={classes.centerElements}>
               <Grid item xs={12} sm>
-                <Link to="/">
-                  <Style.ContainerMenuLogo>
-                    <Style.Img src={logo} alt="company logo" />
-                  </Style.ContainerMenuLogo>
-                </Link>
+                <MediaQuery query="(max-width: 800px)">
+                  <CustomeDrawer />
+                </MediaQuery>
+                <MediaQuery query="(min-width: 801px)">
+                  <Link to="/">
+                    <Style.ContainerMenuLogo>
+                      <Style.Img src={logo} alt="company logo" />
+                    </Style.ContainerMenuLogo>
+                  </Link>
+                </MediaQuery>
               </Grid>
               <Grid item xs={11} sm={6}>
                 <Input type="text" name="search" placeholder="Search products..." />
@@ -175,19 +182,21 @@ export class Menu extends Component {
                 </Popover>
               </Grid>
             </Grid>
-            <div className={classes.linkMenus}>
-              <Grid container>
-                {categories.map(category => {
-                  return (
-                    <Grid xs={12} item sm key={category.id}>
-                      <a key={category.id} href={`/category/${category.id}`}>
-                        {category.name}
-                      </a>
-                    </Grid>
-                  )
-                })}
-              </Grid>
-            </div>
+            <MediaQuery query="(min-width: 800px)">
+              <div className={classes.linkMenus}>
+                <Grid container>
+                  {categories.map(category => {
+                    return (
+                      <Grid xs={12} item sm key={category.id}>
+                        <a key={category.id} href={`/category/${category.id}`}>
+                          {category.name}
+                        </a>
+                      </Grid>
+                    )
+                  })}
+                </Grid>
+              </div>
+            </MediaQuery>
           </Style.Container>
         </Style.ContainerMenu>
       </div>
